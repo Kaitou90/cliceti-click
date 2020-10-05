99c6b37487b30f7dab874df036085648bd80616a
Krista Wikstroem <wikstrom.krista@gmail.com>
2020-10-05 10:37:01 -0400

    added price increase param to init. created get info to btn hover action.

--- app/building.py
+++ app/building.py
@@ -9,6 +9,7 @@ class Building():
 		self.alignment = alignment
 		self.image = image
 		self.boosts = boosts
+		self.priceIncrease = 0.02
 	
 	def buy_buildings(self, money, multiplied=1, priceIncrease=0.02):
 		price = building_cost(multiplied, priceIncrease)
@@ -28,10 +29,28 @@ class Building():
 
 	# count how much coins this type of buildings are producing total per second
 	def coins_counter(self):
-		return self.coinPerBuilding * self.count
+		return self.coinsPerBuilding * self.count
 	
-	def get_building_info():
-		pass
+	def get_building_info(self):
+		maxOption = 12
+		smartOption = 9
+		buyOne = self.building_cost(1, self.priceIncrease)
+		buyTen = self.building_cost(10, self.priceIncrease)
+		buyHundred = self.building_cost(100, self.priceIncrease)
+		buyMax = self.building_cost(maxOption, self.priceIncrease)
+		buySmart = self.building_cost(smartOption, self.priceIncrease)
+
+		hoverText = """{0}
+cost for 1: {3}
+cost for 10: {4}
+cost for 100: {5}
+cost for {1} (MAX): {6}
+cost for {2} (SMART): {7}
+
+Produces {8} coins per second.
+All buildings produce {9} coins per second. ({10})
+"""
+		return hoverText.format(self.name.upper(), maxOption, smartOption, buyOne, buyTen, buyHundred, buyMax, buySmart, self.coinsPerBuilding, self.coins_counter(), '100%')
 
 	def get_all_buildings_info():
 		pass
